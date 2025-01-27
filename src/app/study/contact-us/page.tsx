2c9e13d8801fbad3712ad54f26fca0e9b59ecd20
Tharindu Jayasinghe <jmtjayasinghe.250@gmail.com>
2025-01-27 17:33:43 +0530

    fonts

--- src/app/study/contact-us/page.tsx
+++ src/app/study/contact-us/page.tsx
@@ -5,6 +5,12 @@ export default function Page() {
   return (
     <div>
       <h1>Contact Us Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum fugiat
+        sint eaque dolores saepe esse numquam facilis, ullam voluptate. Earum
+        pariatur saepe omnis perspiciatis unde expedita quae sint officiis.
+        Eveniet.
+      </p>
       <Link href={`/study`} className={styles.link}>
         Back to Home page
       </Link>
